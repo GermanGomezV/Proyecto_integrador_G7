d3aead8fb2a5b2e11044c5abb7d66b93bdbb3b98
GermanGomezV <ger_7214@hotmail.com>
2021-06-14 19:00:22 -0300

    fileName

--- src/models/Users.js
+++ src/models/Users.js
@@ -9,34 +9,35 @@ const fs = require('fs');
 const Users = {
     
     //BD de los usuarios registrados
-    filename: './database/users.json',
+    fileName: './database/users.json',
 
     //Lee el archivo JSON y lo convierte en un objeto literal
     //Luego, lo convierte en un array de objetos literales para trabajarlo con JS
     getData: () => {
-        return JSON.parse(fs.readFileSync(this.filename, 'utf-8'));
+        return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
     },
 
     //Trae todos los usuarios en formato de array de objetos literales
-    findAll : () => {
+    findAll: () => {
         return this.getData();
     },
 
     //Filtra los usuarios por ID y devuelve el objeto literal con el ID indicado
-    findByPk : (id) => {
+    findByPk: (id) => {
         let allUsers = this.findAll();
         let userFound = allUsers.find(usuario => usuario.id === id);
+        return userFound;
     },
 
     //Filtra los usuarios por un texto, que puede ser un ID o un e-mail, buscando solamente en la propiedad (field) indicada.
-    findByField : (field, text) => {
+    findByField: (field, text) => {
         let allUsers = this.findAll();
         let userFound = allUsers.find(usuario => usuario[field] === text);
         return userFound
     },
     
     //Trae el último usuario y le suma uno para agregarlo en la próxima posición del array
-    generateId : () => {
+    generateId: () => {
         let allUsers = this.findAll();
         let lastUser = allUsers.pop();
         if (lastUser) {
@@ -46,22 +47,22 @@ const Users = {
     },
 
     //Crea el usuario y lo agrega en la BD
-    create : (userData) => {
+    create: (userData) => {
         let allUsers = this.findAll();
         let newUser = {
-            id : this.generateId(),
+            id: this.generateId(),
             ...userData
         };
         allUsers.push(newUser);
-        fs.writeFileSync(this.filename, JSON.stringify(allUsers, null, ' '));
+        fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, ' '));
         return newUser;
     },
 
     //Borra al usuario
-    delete : (id) => {
+    delete: (id) => {
         let allUsers = this.findAll();
         let finalUsers = allUsers.filter(usuario => usuario.id !== id);
-        fs.writeFileSync(this.filename, JSON.stringify(finalUsers, null, ' '));
+        fs.writeFileSync(this.fileName, JSON.stringify(finalUsers, null, ' '));
         return true;
     },
 
@@ -73,7 +74,7 @@ const Users = {
             ...userData
         };
         allUsers.push(newUser);
-        fs.writeFileSync(this.filename, JSON.stringify(allUsers, null, ' '));
+        fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, ' '));
         return newUser;
     }
 
