080e1c1354933d65d00d1e18984aff2ce7c61d9e
francoiturco <francoiturco@gmail.com>
2021-08-25 19:36:40 -0300

    edicion del producto funcionando

--- src/middlewares/validacionEdit.js
+++ src/middlewares/validacionEdit.js
@@ -10,19 +10,6 @@ let validaciones = [
     body('descripcion').notEmpty().withMessage('Tienes que escribir una descripccion del producto'),
     body('precio').notEmpty().withMessage('Tienes que establecer un precio de venta'),
     body('descuento').notEmpty().withMessage('Tienes que establecer un descuento de venta'),
-    body('imagen').custom((value, { req }) => {
-        let file = req.file;
-        let acceptedExt = ['.jpg', '.PNG'];
-        if (!file){
-            throw new Error('Tienes que subir una imagen del producto');
-        } else {
-            let fileExt = path.extname(file.originalname);
-            if(!acceptedExt.includes(fileExt)){
-                throw new Error(`Las extensiones para imagenes permitidas son ${acceptedExt.join(', ')}`);
-            }
-        }
-        return true;
-    }),
     body('categoria').notEmpty().withMessage('Tienes que seleccionar la categoria del producto')
 ];
 
