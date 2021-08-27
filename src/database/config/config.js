8dfb135b313ff6bbbc75ad16040f72f07f36536e
GermanGomezV <ger_7214@hotmail.com>
2021-08-27 10:39:52 -0300

    Instalación de React

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
-    "database": "all meet",
+    "password": null,
+    "database": "all_meet",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
