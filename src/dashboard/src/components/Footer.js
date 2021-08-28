8cb47f66ad3fafa0b6f2926f627f01461925056a
francoiturco <francoiturco@gmail.com>
2021-08-28 16:54:44 -0300

    detalles de admin

--- src/dashboard/src/components/Footer.js
+++ src/dashboard/src/components/Footer.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import logo from "../assets/images/logo.png"
 
 function Footer(){
     return (
         <React.Fragment>
-			<footer className="sticky-footer bg-white">
-				<div className="container my-auto">
-					<div className="copyright text-center my-auto">
-						<span>Copyright &copy; Dashboard 2021</span>
-					</div>
+			<footer>
+				<div className="iconos-footer">
+					<i class="fab fa-instagram"></i>
+					<i class="fab facebook fa-facebook-f"></i>
+					<i class="fab fa-twitter"></i>
+				</div>
+				<div className="logo-footer">
+          			<img src={logo} alt="Logo All Meet" />
+        		</div>
+				<div className="derechos">
+					Allmeet 2021. Todos los derechos reservados
 				</div>
 			</footer>
-
         </React.Fragment>
     )
 }
