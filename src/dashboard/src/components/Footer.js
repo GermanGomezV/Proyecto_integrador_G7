eee47cb62680117e583511f86de5fc4b7852acac
francoiturco <francoiturco@gmail.com>
2021-08-28 19:21:39 -0300

    primeras cosas react

--- src/dashboard/src/components/Footer.js
+++ src/dashboard/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from "../assets/images/logo.png"
+import './Footer.css'
 
 function Footer(){
     return (
@@ -10,9 +11,9 @@ function Footer(){
 					<i class="fab facebook fa-facebook-f"></i>
 					<i class="fab fa-twitter"></i>
 				</div>
-				<div className="logo-footer">
-          			<img src={logo} alt="Logo All Meet" />
-        		</div>
+				<div class="logo-footer">
+        			<img class="logo-f" src={logo} alt="All Meet"/>
+    			</div>
 				<div className="derechos">
 					Allmeet 2021. Todos los derechos reservados
 				</div>
