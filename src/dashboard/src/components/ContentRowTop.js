ef1f11f804daac3f18ee9724aa2802871da21ab1
francoiturco <francoiturco@gmail.com>
2021-09-02 09:43:37 -0300

    mejoras en el css

--- src/dashboard/src/components/ContentRowTop.js
+++ src/dashboard/src/components/ContentRowTop.js
@@ -42,7 +42,7 @@ class ContentRowTop extends Component {
     
                     <UltimoProducto>
                         <div className="text-center">
-                            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: '40rem'}} src={this.state.imagen} alt="Imagen Producto"/>
+                            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: '20rem'}} src={this.state.imagen} alt="Imagen Producto"/>
                         </div>
                         <p>{this.state.nombre}</p>
                         <p>{this.state.descripcion}</p>
